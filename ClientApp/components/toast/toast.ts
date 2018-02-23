3a24035b5ddf7dc378dbb2200a00df78182142c1
JB <jb@boyce.com.au>
2018-02-23 13:33:48 +1100

    Working version.

---
+++ ClientApp/components/toast/toast.ts
@@ -0,0 +1,24 @@
+﻿import Vue from "vue";
+import { Component } from "vue-property-decorator";
+
+var toastr = require("toastr/toastr");
+
+@Component
+export default class ToastComponent extends Vue {
+	created() {
+		console.log(toastr);
+	}
+
+	mounted() {
+		console.log("toast mounted");
+		this.showToastr();
+	}
+
+	components: {
+	}
+
+	showToastr() {
+		console.log("toastr triggered");
+		toastr.info("Test message");
+	}
+}
---
+++ webpack.config.vendor.js
@@ -0,0 +1,60 @@
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = (env) => {
+	const isDevBuild = !(env && env.prod);
+	const extractCss = new ExtractTextPlugin("vendor/vendor.css");
+
+	return [{
+		stats: { modules: false },
+		resolve: { extensions: [".js"] },
+		entry: {
+			vendor: [
+				"bootstrap/dist/css/bootstrap.css",
+				"font-awesome/css/font-awesome.css",
+				"toastr/build/toastr.css",
+
+				"bootstrap",
+				"event-source-polyfill",
+				"isomorphic-fetch",
+				"jquery",
+				"popper.js",
+				"toastr",
+				"vue",
+				"vue-router",
+			],
+		},
+		module: {
+			rules: [
+				{ test: /\.css(\?|$)/, use: extractCss.extract({ use: isDevBuild ? "css-loader" : "css-loader?minimize" }) },
+				{ test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: [{ loader: "url-loader", options: { limit: 100000, name: "[hash].[ext]", outputPath: "vendor/", publicPath: "" } }] },
+				// if less than 10 kb, add base64 encoded image to css. if more than 10 kb move to this folder in build using file-loader
+			]
+		},
+		output: {
+			path: path.join(__dirname, "wwwroot"),
+			publicPath: "",
+			filename: "vendor/[name].js",
+			library: "[name]_[hash]"
+		},
+		plugins: [
+			extractCss,
+			new webpack.ProvidePlugin({
+				$: "jquery",
+				jQuery: "jquery",
+				'window.jQuery': "jquery",
+				Popper: ["popper.js", "default"],
+			}), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
+			new webpack.DefinePlugin({
+				'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
+			}),
+			new webpack.DllPlugin({
+				path: path.join(__dirname, "wwwroot", "vendor", "[name]-manifest.json"),
+				name: "[name]_[hash]"
+			})
+		].concat(isDevBuild ? [] : [
+			new webpack.optimize.UglifyJsPlugin()
+		])
+	}];
+};
